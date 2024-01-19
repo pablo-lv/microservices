5b5c93517cdbd4922a41eb332e64598f458c3aae
plucas <pablo.lucv@gmail.com>
2024-01-19 13:27:03 -0600

    Added input validation adn custom exception

--- accounts/src/main/java/com/plucas/accounts/dto/AccountsDTO.java
+++ accounts/src/main/java/com/plucas/accounts/dto/AccountsDTO.java
@@ -1,10 +1,19 @@
 package com.plucas.accounts.dto;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDTO {
+
+    @NotEmpty(message = "AccountNumber cannot be empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "AccountNumber should 10 digits")
     private Long accountNumber;
+
+    @NotEmpty(message = "AccountType cannot be empty")
     private String accountType;
+
+    @NotEmpty(message = "BranchAddress cannot be empty")
     private String branchAddress;
 }
--- accounts/src/main/java/com/plucas/accounts/dto/CustomerDTO.java
+++ accounts/src/main/java/com/plucas/accounts/dto/CustomerDTO.java
@@ -1,14 +1,24 @@
 package com.plucas.accounts.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
-public class CustomerDTO{
+public class CustomerDTO {
 
+    @NotEmpty(message = "Name cannot be empty")
+    @Size(min = 5, max=30, message = "Name should have at least 5 characters")
     private String name;
 
+    @NotEmpty(message = "Email cannot be empty")
+    @Email(message = "Email should be valid")
     private String email;
 
+    @NotEmpty(message = "Mobile number cannot be empty")
+    @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number should 10 digits")
     private String mobileNumber;
 
     private AccountsDTO accountsDTO;
