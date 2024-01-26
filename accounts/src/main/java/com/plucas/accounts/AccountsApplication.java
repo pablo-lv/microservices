e09c47cc69e71e33dab762ee0e455ea9298f6d47
plucas <pablo.lucv@gmail.com>
2024-01-26 12:20:43 -0600

    Added new conyainer build

--- accounts/src/main/java/com/plucas/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/plucas/accounts/AccountsApplication.java
@@ -1,9 +1,11 @@
 package com.plucas.accounts;
 
+import com.plucas.accounts.dto.AccountContactInfoDTO;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
@@ -13,6 +15,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 		version = "1.0",
 		description = "Documentation Accounts API v1.0"
 ))
+@EnableConfigurationProperties(value = {AccountContactInfoDTO.class})
 public class AccountsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AccountsApplication.class, args);
--- accounts/src/main/java/com/plucas/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/plucas/accounts/controller/AccountsController.java
@@ -1,6 +1,7 @@
 package com.plucas.accounts.controller;
 
 import com.plucas.accounts.constants.AccountsConstants;
+import com.plucas.accounts.dto.AccountContactInfoDTO;
 import com.plucas.accounts.dto.CustomerDTO;
 import com.plucas.accounts.dto.ErrorResponseDTO;
 import com.plucas.accounts.dto.ResponseDTO;
@@ -13,7 +14,9 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Pattern;
-import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -23,11 +26,23 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Accounts", description = "CRUD Rest APIs for Accounts")
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
-@AllArgsConstructor
 @Validated
 public class AccountsController {
 
-    private IAccountsService iAccountsService;
+    private final IAccountsService iAccountsService;
+
+    @Value("${build.version}")
+    private String buildVersion;
+
+    @Autowired
+    private Environment environment;
+
+    @Autowired
+    private AccountContactInfoDTO accountContactInfoDTO;
+
+    public AccountsController(IAccountsService iAccountsService) {
+        this.iAccountsService = iAccountsService;
+    }
 
     @Operation(summary = "Create Account", description = "REST API to create account")
     @ApiResponse(responseCode = "201", description = "Account created successfully")
@@ -78,5 +93,22 @@ public class AccountsController {
 
         return ResponseEntity.status(HttpStatus.EXPECTATION_FAILED)
                 .body(new ResponseDTO(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_DELETE, mobileNumber));
+    }
+
+    @GetMapping("/build-info")
+    public ResponseEntity<String> getBuildVersion() {
+        return ResponseEntity.status(HttpStatus.OK).body(buildVersion);
+    }
+
+    @GetMapping("/java-version")
+    public ResponseEntity<String> getJavaVersion() {
+        return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+    }
+
+    @GetMapping("/contact-info")
+    public ResponseEntity<AccountContactInfoDTO> getContactInfo() {
+        return ResponseEntity.status(HttpStatus.OK).body(
+            accountContactInfoDTO
+        );
     }
 }
