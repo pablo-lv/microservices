abae4560c1eb9c9d7d5bbf4d9969a9a94c310685
plucas <pablo.lucv@gmail.com>
2024-01-29 08:10:18 -0600

    Added webhook, spring cloud monitor and bus

--- accounts/src/main/java/com/plucas/accounts/dto/AccountContactInfoDTO.java
+++ accounts/src/main/java/com/plucas/accounts/dto/AccountContactInfoDTO.java
@@ -1,11 +1,18 @@
 package com.plucas.accounts.dto;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 
 @ConfigurationProperties(prefix = "accounts")
-public record AccountContactInfoDTO(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter @Setter
+public class AccountContactInfoDTO {
+
+    private String message;
+    private Map<String, String> contactDetails;
+    private List<String> onCallSupport;
 
 }
