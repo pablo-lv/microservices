b00f62caab2211626be492e1faab593a7890c3f3
plucas <pablo.lucv@gmail.com>
2024-02-06 15:48:25 -0600

    fix docker compose

--- accounts/src/main/java/com/plucas/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/plucas/accounts/controller/AccountsController.java
@@ -6,6 +6,7 @@ import com.plucas.accounts.dto.CustomerDTO;
 import com.plucas.accounts.dto.ErrorResponseDTO;
 import com.plucas.accounts.dto.ResponseDTO;
 import com.plucas.accounts.service.IAccountsService;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -112,9 +113,16 @@ public class AccountsController {
         return ResponseEntity.status(HttpStatus.OK).body("Fallback: " + "0.9");
     }
 
+
+    @RateLimiter(name = "getJavaVersion", fallbackMethod = "getJavaVersionFallback")
     @GetMapping("/java-version")
     public ResponseEntity<String> getJavaVersion() {
         return ResponseEntity.status(HttpStatus.OK).body(environment.getProperty("JAVA_HOME"));
+    }
+
+    public ResponseEntity<String> getJavaVersionFallback(Throwable throwable) {
+        logger.debug("getBuildVersionFallback() method invoked");
+        return ResponseEntity.status(HttpStatus.OK).body("Fallback: " + "Java 17");
     }
 
     @GetMapping("/contact-info")
--- pushImages.sh
+++ pushImages.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Push the Docker image to docker.io
-docker image push docker.io/pablolucv/configserver:s9
+docker image push docker.io/pablolucv/configserver:s10
 echo "Config Server pushed to docker.io"
-docker image push docker.io/pablolucv/eurekaserver:s9
+docker image push docker.io/pablolucv/eurekaserver:s10
 echo "Eureka Server pushed to docker.io"
-docker image push docker.io/pablolucv/accounts:s9
+docker image push docker.io/pablolucv/accounts:s10
 echo "Accounts pushed to docker.io"
-docker image push docker.io/pablolucv/cards:s9
+docker image push docker.io/pablolucv/cards:s10
 echo "Cards pushed to docker.io"
-docker image push docker.io/pablolucv/loans:s9
+docker image push docker.io/pablolucv/loans:s10
 echo "Loans pushed to docker.io"
-docker image push docker.io/pablolucv/gatewayserver:s9
+docker image push docker.io/pablolucv/gatewayserver:s10
 echo "Gateway Server pushed to docker.io"
