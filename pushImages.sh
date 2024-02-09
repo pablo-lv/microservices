bc725fa6f414acfe1c0742fd0e069519d8a8fb91
plucas <pablo.lucv@gmail.com>
2024-02-08 18:04:28 -0600

    Added keycloak auth service

--- pushImages.sh
+++ pushImages.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Push the Docker image to docker.io
-docker image push docker.io/pablolucv/configserver:s11
+docker image push docker.io/pablolucv/configserver:s12
 echo "Config Server pushed to docker.io"
-docker image push docker.io/pablolucv/eurekaserver:s11
+docker image push docker.io/pablolucv/eurekaserver:s12
 echo "Eureka Server pushed to docker.io"
-docker image push docker.io/pablolucv/accounts:s11
+docker image push docker.io/pablolucv/accounts:s12
 echo "Accounts pushed to docker.io"
-docker image push docker.io/pablolucv/cards:s11
+docker image push docker.io/pablolucv/cards:s12
 echo "Cards pushed to docker.io"
-docker image push docker.io/pablolucv/loans:s11
+docker image push docker.io/pablolucv/loans:s12
 echo "Loans pushed to docker.io"
-docker image push docker.io/pablolucv/gatewayserver:s11
+docker image push docker.io/pablolucv/gatewayserver:s12
 echo "Gateway Server pushed to docker.io"
